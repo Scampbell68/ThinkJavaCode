96ca8db7f572747c191c9ecfaf9fbe0c56b6e398
Scampbell68 <stephenlcampbell1968@gmail.com>
2018-07-26 16:25:01 -0500

    comparison exercise

--- ch05/Logic.java
+++ ch05/Logic.java
@@ -6,5 +6,12 @@ public class Logic
         boolean no =false;
         System.out.println("Both YesYes True:"+(yes && yes));
         System.out.println("Both YesNot True:" +(yes && no));
+
+        System.out.println("Either YesYes True: "+(yes||yes));
+        System.out.println("Either YesNo True: "+(yes||no));
+        System.out.println("Either NoNo True: "+(no||no));
+
+        System.out.println("Original Yes Value: "+yes);
+        System.out.println("Inverse Yes Value: "+!yes);
     }
 }
