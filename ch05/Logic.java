69ae792bb6b16048ad40b1cc4fa9338728c0ab43
Scampbell68 <stephenlcampbell1968@gmail.com>
2018-07-26 16:14:10 -0500

    comparison exercise

---
+++ ch05/Logic.java
@@ -0,0 +1,10 @@
+public class Logic
+{
+    public static void main(String[] args)
+    {
+        boolean yes=true;
+        boolean no =false;
+        System.out.println("Both YesYes True:"+(yes && yes));
+        System.out.println("Both YesNot True:" +(yes && no));
+    }
+}
